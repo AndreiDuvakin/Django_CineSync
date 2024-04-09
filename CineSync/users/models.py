f60efd4052027514a18f5441a2cd3e142e4d0213
Андрей <andrei@duvakin.ru>
2024-04-09 20:22:22 +0500

    сделал конфиг проекта и зависимости

--- CineSync/users/models.py
+++ CineSync/users/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from django.conf import settings
+from django.db.models import Model, OneToOneField, CASCADE, DateField, CharField
 
-# Create your models here.
+
+class Profile(Model):
+    user = OneToOneField(
+        settings.AUTH_USER_MODEL,
+        verbose_name='пользователь',
+        related_name='profile',
+        related_query_name='profile',
+        on_delete=CASCADE,
+    )
+    birthday = DateField(
+        null=True,
+        blank=True,
+    )
+    role = CharField(
+        verbose_name='роль пользователя',
+    )
