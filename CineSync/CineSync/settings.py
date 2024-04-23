a5cda0bd0be7f49c14f423f9b7ff8d01c72e3ffb
andrei <andrei@duvakin.ru>
2024-04-23 22:02:08 +0500

    поправил сетинг

--- CineSync/CineSync/settings.py
+++ CineSync/CineSync/settings.py
@@ -120,9 +120,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
